248a1d7ad6dadacb755d4f3740f14be9738c104a
miawithcode <cuttontail@gmail.com>
2023-12-20 12:20:46 +0800

    refactor: iterate link list

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Backroads
+
+## Smooth Scroll
+用 Tailwind 添加`<html>`标签的样式：
+```css
+@layer base {
+  html {
+    scroll-behavior: smooth;
+  }
+  .section {
+    /* Navbar Height */
+    scroll-margin-top: 4rem;
+  }
+}
+```
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from '../images/logo.svg';
+import { pageLinks, socialLinks } from '../data';
 
 function Navbar() {
   return (
@@ -12,63 +13,34 @@ function Navbar() {
         </div>
 
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link">
-              {' '}
-              home{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#about" className="nav-link">
-              {' '}
-              about{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link">
-              {' '}
-              services{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link">
-              {' '}
-              tours
-            </a>
-          </li>
+          {pageLinks.map((link) => {
+            const { id, href, text } = link;
+            return (
+            <li key={id}>
+              <a href={href} className="nav-link">
+                {text}
+              </a>
+            </li>
+            )
+          })}
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
+          {socialLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
