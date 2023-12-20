484f2c5a4c9468ba16fab31fac80472bc7c882cf
miawithcode <cuttontail@gmail.com>
2023-12-20 12:43:50 +0800

    refactor: set service data and iterate repeat element in service component

--- src/data.js
+++ src/data.js
@@ -10,3 +10,27 @@ export const socialLinks = [
   { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
   { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 1,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+];
